4c1fe55dd8629b9fc5c529a385a0b649e82c0c7c
annaabr <annaabramyan2013@gmail.com>
2024-11-11 13:33:10 +0300

    done

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import asyncio
+import random
+import aiohttp  # Библиотека для асинхронных HTTP-запросов
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
-from config import TOKEN
+from config import TOKEN, WEATHER_API_KEY  # Добавьте WEATHER_API_KEY в ваш файл config.py
 
-import random
-
-bot = Bot(token = TOKEN)
+bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
 
 @dp.message(Command('photo'))
 async def photo(message: Message):
@@ -19,6 +20,7 @@ async def photo(message: Message):
     random_photo = random.choice(list)
     await message.answer_photo(random_photo, caption='Это красивая картинка')
 
+
 @dp.message(F.photo)
 async def react_photo(message: Message):
     list = ['Непонятно, что это такое', 'Не отправляй мне такое больше!', 'Ого! Какая красивая фотка!']
@@ -28,16 +30,39 @@ async def react_photo(message: Message):
 
 @dp.message(F.text == 'Что такое ИИ?')
 async def help(message: Message):
-    await message.answer('Искусственный интеллект (англ. artificial intelligence; AI) в самом широком смысле — это интеллект, демонстрируемый машинами, в частности компьютерными системами.')
+    await message.answer(
+        'Искусственный интеллект (англ. artificial intelligence; AI) в самом широком смысле — это интеллект, демонстрируемый машинами, в частности компьютерными системами.')
 
 
 @dp.message(Command('help'))
 async def help(message: Message):
-    await message.answer('Этот бот умеет выполнять команды:\n /start \n /help \n /photo')
+    await message.answer('Этот бот умеет выполнять команды:'
+                         '\n /start '
+                         '\n /help '
+                         '\n /photo '
+                         '\n /weather' 
+                         '\n Бот также умеет отвечать на вопрос "Что такое ИИ?"')
+
 
 @dp.message(CommandStart())
 async def start(message: Message):
     await message.answer('Приветики! Я бот!')
+
+
+@dp.message(Command('weather'))
+async def weather(message: Message):
+    city = 'Rostov-on-Don'  # Укажите город
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_API_KEY}&units=metric&lang=ru'
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                data = await response.json()
+                temp = data['main']['temp']
+                weather_description = data['weather'][0]['description']
+                await message.answer(
+                    f'Погода в Ростове-на-Дону:\nТемпература: {temp}°C\nОписание: {weather_description.capitalize()}')
+            else:
+                await message.answer('Не удалось получить данные о погоде.')
 
 async def main():
     await dp.start_polling(bot)
